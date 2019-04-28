b89ab19b48e4f050b0f3ad189fa70f13826a8feb
LapinsMorts <simon-2.lacasse@polymtl.ca>
2019-04-28 19:09:11 -0400

    add sync of battery

--- INF8405/app/src/main/java/ca/polymtl/inf8405/Main/StatsView/StatsViewFragment.kt
+++ INF8405/app/src/main/java/ca/polymtl/inf8405/Main/StatsView/StatsViewFragment.kt
@@ -1,6 +1,9 @@
 package ca.polymtl.inf8405.Main.StatsView
 
 
+import android.content.Intent
+import android.content.IntentFilter
+import android.os.BatteryManager
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
@@ -8,6 +11,8 @@ import android.view.View
 import android.view.ViewGroup
 
 import ca.polymtl.inf8405.R
+import kotlinx.android.synthetic.main.fragment_stats_view.view.*
+import java.util.*
 
 class StatsViewFragment : Fragment() {
 
@@ -16,7 +21,32 @@ class StatsViewFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_stats_view, container, false)
+        val view = inflater.inflate(R.layout.fragment_stats_view, container, false)
+        updateBatteryView(view)
+        Timer().scheduleAtFixedRate(object: TimerTask(){
+            override fun run() {
+                updateBatteryView(view)
+            }
+        }, 0, 1000)
+        return view
+    }
+
+    fun updateBatteryView(view: View) {
+        val batteryStatus: Intent? = IntentFilter(Intent.ACTION_BATTERY_CHANGED).let { ifilter ->
+            context?.registerReceiver(null, ifilter)
+        }
+        val batteryPct: Float = batteryStatus?.let { intent ->
+            val level: Int = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1)
+            val scale: Int = intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1)
+            level / scale.toFloat()
+        } ?: 0.0f
+        var batteryLevel = (batteryPct * 100).toInt()
+        view.batteryMeterView.chargeLevel = batteryLevel
+        view.batteryLevelText.text = "$batteryLevel%"
+        val status: Int = batteryStatus?.getIntExtra(BatteryManager.EXTRA_STATUS, -1) ?: -1
+        view.batteryMeterView.isCharging = status == BatteryManager.BATTERY_STATUS_CHARGING
+                || status == BatteryManager.BATTERY_STATUS_FULL
+
     }
 
 
