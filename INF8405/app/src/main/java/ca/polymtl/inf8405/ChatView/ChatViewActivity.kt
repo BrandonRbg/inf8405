153f526035c2124a0247d87a24a0cf7bd08cce87
Brandon Roberge <brandonroberge@hotmail.ca>
2019-04-28 15:04:28 -0400

    Stuff

--- INF8405/app/src/main/java/ca/polymtl/inf8405/ChatView/ChatViewActivity.kt
+++ INF8405/app/src/main/java/ca/polymtl/inf8405/ChatView/ChatViewActivity.kt
@@ -1,5 +1,10 @@
 package ca.polymtl.inf8405.ChatView
 
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
@@ -7,29 +12,46 @@ import ca.polymtl.inf8405.BR
 import ca.polymtl.inf8405.Domain.Message
 import ca.polymtl.inf8405.R
 import ca.polymtl.inf8405.Services.MessagingService
+import ca.polymtl.inf8405.Utils.buildDrawer
 import ca.polymtl.inf8405.Utils.currentUser
 import com.github.nitrico.lastadapter.BaseType
 import com.github.nitrico.lastadapter.LastAdapter
 import com.github.nitrico.lastadapter.TypeHandler
 import com.google.firebase.firestore.ListenerRegistration
+import com.mikepenz.materialdrawer.Drawer
 import dagger.android.AndroidInjection
 import kotlinx.android.synthetic.main.activity_chat_view.*
 import javax.inject.Inject
 
-class ChatViewActivity : AppCompatActivity() {
+class ChatViewActivity : AppCompatActivity(), SensorEventListener {
+
     @Inject
     lateinit var messagingService: MessagingService
 
     lateinit var adapter: LastAdapter
+    lateinit var sensorManager: SensorManager
+    var lightSensor: Sensor? = null
+    var gravitySensor: Sensor? = null
+    var currentLight: Float = 0.0f
+    var currentGravity: Float = 0.0f
 
     var messageListenerRegistration: ListenerRegistration? = null
-
     var messages = mutableListOf<Message>()
+
+    private lateinit var drawer: Drawer
 
     override fun onCreate(savedInstanceState: Bundle?) {
         AndroidInjection.inject(this)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat_view)
+
+        setSupportActionBar(toolbar)
+
+        drawer = buildDrawer(toolbar, 0)
+
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        lightSensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT)
+        gravitySensor = sensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY)
 
         messagesRv.layoutManager = LinearLayoutManager(this).apply {
             stackFromEnd = true
@@ -57,11 +79,35 @@ class ChatViewActivity : AppCompatActivity() {
                 messageEdit.setText("")
             }
         }
+
+        temperatureButton.setOnClickListener {
+            if (lightSensor == null) {
+                messageEdit.setText("Premièrement, ton device supporte pas la lumière")
+            } else {
+                messageEdit.setText("Premièrement, check comment que mon device est lumineux: $currentLight lx")
+            }
+        }
+
+        gravityButton.setOnClickListener {
+            if (gravitySensor == null) {
+                messageEdit.setText("Ton device supporte pas la gravité")
+            } else {
+                messageEdit.setText("Check ma gravité! $currentGravity m/s2")
+            }
+        }
     }
 
-    override fun onStop() {
-        super.onStop()
+    override fun onPause() {
+        super.onPause()
         messageListenerRegistration?.remove()
+        sensorManager.unregisterListener(this)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        listenForMessages()
+        sensorManager.registerListener(this, lightSensor, SensorManager.SENSOR_DELAY_FASTEST)
+        sensorManager.registerListener(this, gravitySensor, SensorManager.SENSOR_DELAY_FASTEST)
     }
 
     private fun listenForMessages() {
@@ -71,6 +117,27 @@ class ChatViewActivity : AppCompatActivity() {
 
             adapter.notifyDataSetChanged()
             messagesRv.scrollToPosition(messages.size - 1)
+        }
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+        // NO-OP
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        event?.also {
+            when (it.sensor.type) {
+                Sensor.TYPE_LIGHT -> currentLight = it.values[0]
+                Sensor.TYPE_GRAVITY -> currentGravity = Math.sqrt(it.values.map { it * it }.sum().toDouble()).toFloat()
+            }
+        }
+    }
+
+    override fun onBackPressed() {
+        if (drawer.isDrawerOpen) {
+            drawer.closeDrawer()
+        } else {
+            super.onBackPressed()
         }
     }
 }
---
+++ INF8405/app/src/main/java/ca/polymtl/inf8405/Utils/MaterialDrawerBuilder.kt
@@ -0,0 +1,18 @@
+package ca.polymtl.inf8405.Utils
+
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+import ca.polymtl.inf8405.R
+import com.mikepenz.materialdrawer.DrawerBuilder
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem
+
+fun AppCompatActivity.buildDrawer(toolbar: Toolbar, selectedItem: Int) = DrawerBuilder()
+    .withActivity(this)
+    .withToolbar(toolbar)
+    .addDrawerItems(
+        PrimaryDrawerItem().withName("Chat").withIcon(R.drawable.ic_message_black_24dp),
+        PrimaryDrawerItem().withName("Map").withIcon(R.drawable.ic_map_black_24dp),
+        PrimaryDrawerItem().withName("Stats").withIcon(R.drawable.ic_data_usage_black_24dp)
+    )
+    .withSelectedItemByPosition(selectedItem)
+    .build()
